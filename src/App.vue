f2c9cde7b49efd0d978cb1b9e7b9842d6e6f09f3
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-18 17:26:28 +0100

    Add component about results achieved

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import HeaderComp from "./components/HeaderComp.vue";
+import ResultsComp from "./components/ResultsComp.vue";
 
 export default {
   components: {
     HeaderComp,
+    ResultsComp,
   },
 };
 </script>
 
 <template>
   <HeaderComp />
+  <ResultsComp />
 </template>
 
 <style lang="scss">
---
+++ src/components/ResultsComp.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "ResultsComp",
+};
+</script>
+
+<template>
+  <div class="container py-5">
+    <div class="row text-center">
+      <div class="col-3">
+        <h3>1.926</h3>
+        <p>FINISHED SESSIONS</p>
+      </div>
+      <div class="col-3">
+        <h3>100%</h3>
+        <p>SATISFACTION RATE</p>
+      </div>
+      <div class="col-3">
+        <h3>3092+</h3>
+        <p>ENROLLED LEARNERS</p>
+      </div>
+      <div class="col-3">
+        <h3>200</h3>
+        <p>ONLINE INSTRUCTORS</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
