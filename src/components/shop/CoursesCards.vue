b1e04436f55bee4d98f07adee1e24c1c95332ec5
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-18 19:31:37 +0100

    Add online courses selling section

---
+++ src/components/shop/CoursesCards.vue
@@ -0,0 +1,26 @@
+<script>
+import CTAbutton from "../CTAbutton.vue";
+
+export default {
+  name: "CoursesCards",
+  components: {
+    CTAbutton,
+  },
+};
+</script>
+
+<template>
+  <div class="card" v-for="n in 6">
+    <img
+      class="img-fluid"
+      src="../../assets/styles/images/motivation-course-06-480x298.jpg"
+      alt="" />
+    <div class="card-content p-4">
+      <h5 class="card-price"><span class="big-price">$30</span>.00</h5>
+      <h6>How to be Succesful: Create A Growth Mindset For Success</h6>
+      <span>3 Lessons</span> <span>50 Students</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/shop/CoursesShop.vue
@@ -0,0 +1,33 @@
+<script>
+import CoursesCards from "./CoursesCards.vue";
+import CTAbutton from "../CTAbutton.vue";
+
+export default {
+  name: "CoursesShop",
+  components: {
+    CoursesCards,
+    CTAbutton,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-12">
+        <span>Begin Your Journey at Maxcoach</span>
+      </div>
+      <div class="col-12">
+        <h2>Latest <span>Online Courses</span></h2>
+      </div>
+    </div>
+    <div class="col-12 d-flex flex-wrap">
+      <CoursesCards />
+    </div>
+    <!-- <div class="col-12"> -->
+    <!-- <CTAbutton /> -->
+    <!-- </div> -->
+  </div>
+</template>
+
+<style lang="scss"></style>
