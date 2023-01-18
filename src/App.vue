eb8da4b50efb154e3b5c4aed0bea1341146095bf
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-18 16:33:12 +0100

    Create header comp raw base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
+
+export default {
+  components: {
+    HeaderComp,
+  },
+};
+</script>
+
+<template>
+  <HeaderComp />
+</template>
+
+<style lang="scss">
+@use "../node_modules/bootstrap/scss/bootstrap.scss" as *;
+@use "./assets/styles/general.scss" as *;
+@use "./assets/styles/partials/variables" as *;
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  name: "HeaderComp",
+};
+</script>
+
+<template>
+  <div class="container header">
+    <div class="div">
+      <img src="../assets/styles/images/dark-logo.png" alt="Site logo" />
+    </div>
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Pages</a></li>
+        <li><a href="">Courses</a></li>
+        <li><a href="">Features</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Shop</a></li>
+      </ul>
+    </nav>
+    <div class="social-links">
+      <span>Twitter</span>
+      <span>FB</span>
+      <span>IG</span>
+      <span>LinkedIn</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  width: 80%;
+  margin: 0 auto;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding-top: 1rem;
+
+  img {
+    width: 10rem;
+  }
+
+  ul {
+    list-style-type: none;
+
+    li {
+      display: inline;
+
+      a {
+        display: inline-block;
+        padding: 0 1rem;
+        text-decoration: none;
+      }
+    }
+  }
+
+  .social-links span {
+    padding: 0 0.8rem;
+  }
+}
+</style>
