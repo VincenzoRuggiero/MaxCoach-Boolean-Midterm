92b3e1eda559610fdce6f0184cef2fa663d0dbab
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-19 09:34:56 +0100

    Add section for future Carousel component

---
+++ src/components/TestimonialsComp.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "TestimonialsComp",
+};
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row d-flex justify-content-center">
+      <div class="col-6 carousel-bg waves-pattern"></div>
+      <div class="col-6 man"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@use "../assets/styles/partials/variables" as *;
+
+.carousel-bg {
+  background-color: $athens-grey;
+  height: 60vh;
+}
+
+.waves-pattern {
+  background-image: url("../assets/styles/images/background-pattern-wavify.png");
+}
+
+.man {
+  background-image: url("../assets/styles/images/home-movation-testimonial-image-768x562.jpg");
+  background-repeat: no-repeat;
+}
+</style>
