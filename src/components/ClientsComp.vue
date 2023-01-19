c34d90ee705f344fdf83d50a88285134bef4a91e
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-19 15:49:00 +0100

    Clients logo are shown dynamically with opacity applied

--- src/components/ClientsComp.vue
+++ src/components/ClientsComp.vue
@@ -1,47 +1,49 @@
 <script>
 export default {
   name: "ClientsComp",
+  data() {
+    return {
+      logos: [
+        {
+          img: "logo-01",
+        },
+        {
+          img: "logo-02",
+        },
+        {
+          img: "logo-03",
+        },
+        {
+          img: "logo-04",
+        },
+        {
+          img: "logo-05",
+        },
+        {
+          img: "logo-06",
+        },
+      ],
+    };
+  },
+  methods: {
+    getImagePath(image) {
+      return new URL(
+        `../assets/styles/images/client-${image}-primary.png`,
+        import.meta.url
+      ).href;
+    },
+  },
 };
 </script>
 
 <template>
   <div class="container my-5 py-5">
     <div class="row">
-      <div class="col-2">
+      <div class="col-2" v-for="logo in logos">
         <img
-          class="img-fluid opacity-25"
-          src="../assets/styles/images/client-logo-01-primary.png"
-          alt="" />
-      </div>
-      <div class="col-2">
-        <img
-          class="img-fluid"
-          src="../assets/styles/images/client-logo-01-primary.png"
-          alt="" />
-      </div>
-      <div class="col-2">
-        <img
-          class="img-fluid"
-          src="../assets/styles/images/client-logo-01-primary.png"
-          alt="" />
-      </div>
-      <div class="col-2">
-        <img
-          class="img-fluid"
-          src="../assets/styles/images/client-logo-01-primary.png"
-          alt="" />
-      </div>
-      <div class="col-2">
-        <img
-          class="img-fluid"
-          src="../assets/styles/images/client-logo-01-primary.png"
-          alt="" />
-      </div>
-      <div class="col-2">
-        <img
-          class="img-fluid"
-          src="../assets/styles/images/client-logo-01-primary.png"
-          alt="" />
+          class="img-fluid opacity-50"
+          :src="getImagePath(logo.img)"
+          alt="Client logo" />
       </div>
     </div>
   </div>
