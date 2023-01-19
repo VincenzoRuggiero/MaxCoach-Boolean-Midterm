c0fd8afaf816d90cd2e4ad00087c279bc8f64af2
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-19 18:00:10 +0100

    Update Blog section with articles rendered dynamically

--- src/components/blog/ArticleCard.vue
+++ src/components/blog/ArticleCard.vue
@@ -1,18 +1,33 @@
 <script>
 export default {
   name: "ArticleCard",
+  props: ["image", "date", "title", "excerpt"],
 };
 </script>
 
 <template>
-  <div>
-    <img
-      class="img-fluid"
-      src="../../assets/styles/images/motivation-blog-01-480x325.jpg" />
-    <span> May 13, 2020</span>
-    <h6>How to Stay True to Your Personal Brand</h6>
-    <p>When it comes to your business or career, you want ...</p>
+  <div class="article-card">
+    <img class="img-fluid" :src="image" alt="Article featured image" />
+    <span class="grey"> {{ date }}</span>
+    <h6>{{ title }}</h6>
+    <p class="grey">{{ excerpt }}</p>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.article-card {
+  padding: 0 1rem;
+
+  span {
+    display: inline-block;
+    margin-top: 1.3rem;
+    font-size: 0.9rem;
+  }
+
+  h6 {
+    font-size: 1.3rem;
+    line-height: 2rem;
+    padding: 1rem 0;
+  }
+}
+</style>
--- src/components/blog/BlogComp.vue
+++ src/components/blog/BlogComp.vue
@@ -6,20 +6,66 @@ export default {
   components: {
     ArticleCard,
   },
+  data() {
+    return {
+      articles: [
+        {
+          img: "motivation-blog-04-480x325.jpg",
+          date: "May 13, 2020",
+          title: "How to Stay True to Your Personal Brand",
+          excerpt: "When it comes to your business or career, you want ...",
+        },
+        {
+          img: "motivation-blog-03-480x325.jpg",
+          date: "May 13, 2020",
+          title: "5 Vital Lessons in 5 Years of Freelancing",
+          excerpt:
+            "Being self-employed and working from home, it's easy to get ...",
+        },
+        {
+          img: "motivation-blog-02-480x325.jpg",
+          date: "May 13, 2020",
+          title: "11 Super Useful Tips for Small-Business Owners",
+          excerpt:
+            "Being a small-business owner poses a ton of challenges. We ...",
+        },
+        {
+          img: "motivation-blog-01-480x325.jpg",
+          date: "May 13, 2020",
+          title: "How to Give Yourself Grace to Start Again",
+          excerpt:
+            "Forgive yourself for bad habits you may have started or ...",
+        },
+      ],
+    };
+  },
+  methods: {
+    getImagePath(image) {
+      return new URL(`../../assets/styles/images/${image}`, import.meta.url)
+        .href;
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="container py-5">
+  <div class="container mt-5 py-5">
     <div class="row">
       <div class="col-12">
         <span class="section-name">Blog Updated</span>
-        <h2>Interesting <span> articles updated</span> daily</h2>
-        <div class="col-12 d-flex">
-          <ArticleCard v-for="n in 4" />
+        <h2 class="py-3">
+          Interesting <span class="green"> articles updated</span> daily
+        </h2>
+        <div class="col-12 d-flex mt-5">
+          <ArticleCard
+            v-for="article in articles"
+            :image="getImagePath(article.img)"
+            :date="article.date"
+            :title="article.title"
+            :excerpt="article.excerpt" />
         </div>
         <div class="col-12">
-          <div class="row text-center">
+          <div class="row text-center mt-5">
             <a class="section-name green" href="#">View all posts</a>
           </div>
         </div>
@@ -28,4 +74,4 @@ export default {
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped></style>
