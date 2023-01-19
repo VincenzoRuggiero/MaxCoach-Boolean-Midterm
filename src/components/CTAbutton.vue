99e6ecfbe688948e22c0327bd6ae2f7b9f270615
Vincenzo Ruggiero <vincenzoru@live.it>
2023-01-19 11:08:11 +0100

    Style reusable button comp for call to actions

--- src/components/CTAbutton.vue
+++ src/components/CTAbutton.vue
@@ -1,13 +1,26 @@
 <script>
-export default {};
+export default {
+  name: "CTAbutton",
+};
 </script>
 
 <template>
-  <a></a>
+  <a class="button">Get Started For Free <span class="arrow">&#x2192;</span></a>
 </template>
 
-<style lang="scss">
-a {
-  padding: 2rem;
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+
+.button {
+  color: $white;
+  background-color: $mountain-meadow;
+  text-decoration: none;
+  padding: 0.8rem 3rem;
+  font-weight: bold;
+  border-radius: 5px;
+
+  .arrow {
+    margin-left: 0.7rem;
+  }
 }
 </style>
